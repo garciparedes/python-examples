3f529fa227243b0f6226fc7838be197543bfd7d9
Sergio García <garciparedes@gmail.com>
2015-10-10 10:51:55 +0200

    Changed to same probability for 0 and 1 on matrixExample

--- matrixExample.py
+++ matrixExample.py
@@ -7,7 +7,6 @@ matrix = [[0 for x in range(dimension)] for x in range(dimension)]
 for i in range(len(matrix)):
     linea = ""
     for j in range (len(matrix[i])):
-        if (randint(0,1) == 0 and randint(0,1) == 0):
-            matrix [i][j] = randint(0,1)
+        matrix [i][j] = randint(0,1)
         linea += str(matrix[i][j])
     print linea
