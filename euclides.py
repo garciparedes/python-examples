18db2714ca1a1cb93ac585a322d324579617b427
Sergio García <garciparedes@gmail.com>
2015-10-12 12:26:03 +0200

    Euclides Algorithm

---
+++ euclides.py
@@ -0,0 +1,29 @@
+"""
+Author: Sergio Garcia Prado
+        www.garciparedes.me
+
+Example of Euclides Algorithm.
+""""
+
+
+def euclides(m, n):
+    while(m > 0):
+        t = m
+        m = n % m
+        n = t
+    return n
+
+
+def main():
+
+    print ("GCD Euclides Algorithm.")
+    
+    numA = raw_input("Number A: ")
+    numB = raw_input("Number B: ")
+
+    print ("GCD of %s and %s is  %s" % (numA, numB,euclides(3,6)))
+
+
+
+if __name__ == "__main__":
+    main()
