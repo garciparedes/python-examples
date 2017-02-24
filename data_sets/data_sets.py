7fb7538f216dddca33d1481aa1a9b2eed9e5e8b9
Sergio Garcia <sergio@garciparedes.me>
2017-02-24 12:30:18 +0100

    Added new datasets

--- data_sets/data_sets.py
+++ data_sets/data_sets.py
@@ -2,6 +2,11 @@ import pandas as pd
 
 
 class DataSets:
+
+    @staticmethod
+    def _get_path():
+        return "./data_sets"
+
     @staticmethod
     def get_weber_nominal():
         outLook = pd.Series(["Sunny", "Overcast", "Rain"], dtype="category")
@@ -32,9 +37,30 @@ class DataSets:
         return pd.DataFrame(data, columns=columns, dtype="category")
 
     @staticmethod
+    def get_weather():
+        return pd.read_csv(DataSets._get_path() + '/weather.csv')
+
+    @staticmethod
     def get_car_eval():
-        return pd.read_csv('./data_sets/careval.csv')
+        return pd.read_csv(DataSets._get_path() + '/careval.csv')
 
     @staticmethod
     def get_credit():
-        return pd.read_csv('./data_sets/credit.csv')
+        return pd.read_csv(DataSets._get_path() + '/credit.csv')
+
+    @staticmethod
+    def get_presion():
+        pd_class = pd.Series(["-", "+"], dtype="category")
+
+        columns = pd.Index(["Presion", "Clase"])
+
+        data = [
+            40, pd_class[0],
+            48, pd_class[0],
+            60, pd_class[0],
+            72, pd_class[0],
+            80, pd_class[0],
+            90, pd_class[0],
+        ]
+
+        return pd.DataFrame(data, columns=columns)
