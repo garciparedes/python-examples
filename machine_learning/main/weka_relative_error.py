35cde725450bb923e462e0895409fcd60f4183fb
Sergio García Prado <garciparedes@gmail.com>
2017-04-18 11:21:15 +0200

    Update weka_relative_error.py
    improved code style

--- machine_learning/main/weka_relative_error.py
+++ machine_learning/main/weka_relative_error.py
@@ -5,8 +5,7 @@ import pandas as pd
 
 
 if __name__ == "__main__":
-    file_name = sys.argv[1]
+    file_error = pd.read_csv(sys.argv[1])
     error_tase = float(sys.argv[2])
-    file_error = pd.read_csv(file_name)
-    error_count = (file_error['error'].abs() / file_error['actual']<= error_tase).sum()
+    error_count = (file_error['error'].abs() / file_error['actual'] <= error_tase).sum()
     print( error_count / float(file_error.shape[0]))
