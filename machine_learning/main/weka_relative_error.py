b448ba5ee788a9fb28c6d2209b3f451fa3d54223
Sergio Garcia <sergio@garciparedes.me>
2017-04-19 09:43:19 +0200

    Improved Script

--- machine_learning/main/weka_relative_error.py
+++ machine_learning/main/weka_relative_error.py
@@ -5,7 +5,10 @@ import pandas as pd
 
 
 if __name__ == "__main__":
-    file_error = pd.read_csv(sys.argv[1])
-    error_tase = float(sys.argv[2])
-    error_count = (file_error['error'].abs() / file_error['actual'] <= error_tase).sum()
-    print( error_count / float(file_error.shape[0]))
+    file_name = sys.argv[1]
+    error_ratio_list = [float(i) for i in sys.argv[2:]]
+    file_error = pd.read_csv(file_name)
+
+    for error_ratio in error_ratio_list:
+        error_count = (file_error['error'].abs() / file_error['actual']<= error_ratio).sum()
+        print('Max Rel. Error: ' + str(error_ratio) + '\t-> Error Ratio:\t'  + str(error_count / float(file_error.shape[0])))
