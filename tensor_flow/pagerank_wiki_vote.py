db2f5a27608946056b688173db7ad018491839be
Sergio Garcia Prado <sergio@garciparedes.me>
2017-06-11 11:48:56 +0200

    Minor Changes

--- tensor_flow/pagerank_wiki_vote.py
+++ tensor_flow/pagerank_wiki_vote.py
@@ -1,21 +1,28 @@
 import tensorflow as tf
-
+import numpy as np
 from data_sets.data_sets import DataSets
 
 
+def ranked(x):
+    # x will be a numpy array with the contents of the placeholder below
+    return np.argsort(x, axis=0)
+
+
 def main():
-    beta = 0.85
     steps = 20
 
-    data_set = DataSets.get_wiki_vote() - 1
-    n = data_set.max(axis=0).max() + 1
+    data_set = DataSets.get_followers()
+    data_set -= 1
+    n_raw = data_set.max(axis=0).max() + 1
+
+    beta = tf.constant(0.85, tf.float32)
+    n = tf.constant(n_raw, tf.float32)
 
     a = tf.Variable(tf.transpose(
         tf.scatter_nd(data_set.values.tolist(), data_set.shape[0] * [1.0],
-                      [n, n])),
-                    tf.float32)
+                      [n_raw, n_raw])), tf.float64)
 
-    v = tf.Variable(tf.fill([n, 1], tf.cast((1 / n), tf.float32)))
+    v = tf.Variable(tf.fill([n_raw, 1], tf.pow(n, -1)))
 
     o_degree = tf.reduce_sum(a, 0)
 
@@ -24,7 +31,7 @@ def main():
     transition = tf.transpose(
         tf.where(condition,
                  tf.transpose(beta * tf.div(a, o_degree) + (1 - beta) / n),
-                 tf.fill([n, n], tf.cast((1 / n), tf.float32))))
+                 tf.fill([n_raw, n_raw], tf.pow(n, -1))))
 
     page_rank = tf.matmul(transition, v, a_is_sparse=True)
 
@@ -38,7 +45,7 @@ def main():
             sess.run(run_iteration)
 
         print(sess.run(v))
-        print(sess.run(tf.arg_max(v, dimension=0)))
+        print(sess.run(tf.transpose(tf.py_func(ranked, [-v], tf.int64))[0]))
         tf.summary.FileWriter('logs/.', sess.graph)
         pass
 
