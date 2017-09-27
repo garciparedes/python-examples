a924ec084f1d6f4d58d28e29c411f49d7f652b7b
Sergio Garcia Prado <sergio@garciparedes.me>
2017-09-27 15:57:51 +0200

    Added template file for statistics scripts

---
+++ stats/stats_template.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python3
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import *
+
+
+def main():
+
+    # Code Here
+
+    pass
+
+if __name__ == '__main__':
+    main()
