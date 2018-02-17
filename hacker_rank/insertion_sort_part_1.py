7bde39ef7db5558bba814426f9f01ad26c1ab3c6
Sergio Garcia Prado <sergio@garciparedes.me>
2018-02-17 15:19:24 +0100

    Added Insertion Sort: Part 1 solution for HackerRank problem

---
+++ hacker_rank/insertion_sort_part_1.py
@@ -0,0 +1,24 @@
+#!/bin/python3
+
+import sys
+
+
+def insertionSort1(n, arr):
+    elem = arr[len(arr) - 1]
+    i = len(arr)
+    shifted = True
+    while 0 < i and shifted:
+        shifted = False
+        i -= 1
+        if i and elem < arr[i - 1]:
+            arr[i] = arr[i - 1]
+            shifted = True
+        else:
+            arr[i] = elem
+        print(' '.join(map(str, arr)))
+
+
+if __name__ == "__main__":
+    n = int(input().strip())
+    arr = list(map(int, input().strip().split(' ')))
+    insertionSort1(n, arr)
