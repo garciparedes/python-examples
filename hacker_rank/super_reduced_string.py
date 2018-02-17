d9f9870297e86d18ea2bdf838c482332a1f7d9a2
Sergio Garcia Prado <sergio@garciparedes.me>
2018-02-17 14:40:40 +0100

    Added Super Reduced String solution for HackerRank

---
+++ hacker_rank/super_reduced_string.py
@@ -0,0 +1,19 @@
+#!/bin/python3
+
+import sys
+import re
+
+
+def super_reduced_string(s):
+    r = re.sub(r'([a-z])\1', '', s)
+    while r is not s:
+        s = r
+        r = re.sub(r'([a-z])\1', '', s)
+    if not r:
+        r = 'Empty String'
+    return r
+
+
+s = input().strip()
+result = super_reduced_string(s)
+print(result)
