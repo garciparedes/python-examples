a61efe206a083d1d4528d149a0c629883b68eac5
Sergio Garcia Prado <sergio@garciparedes.me>
2018-02-19 17:56:24 +0100

    Added solution for Minimum Absolute Difference in an Array problem of Hacker Rank

---
+++ hacker_rank/minimum_absolute_difference_in_an_array.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+
+import sys
+
+def minimumAbsoluteDifference(n, arr):
+    arr.sort()
+    r = abs(arr[0] - arr[1])
+    for i in range(2, len(arr)):
+        if abs(arr[i] - arr[i - 1]) < r:
+            r = abs(arr[i] - arr[i - 1])
+    return r
+
+
+if __name__ == "__main__":
+    n = int(input().strip())
+    arr = list(map(int, input().strip().split(' ')))
+    result = minimumAbsoluteDifference(n, arr)
+    print(result)
