01ed4d7567b5ec5deb556c4bea0ba6abbb170d0f
Sergio Garcia Prado <sergio@garciparedes.me>
2018-02-21 16:36:15 +0100

    Solution of Maximizing XOR exercise of HackerRank

---
+++ hacker_rank/maximizing_xor.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+
+import sys
+
+def maximizingXor(l, r):
+    a = 0
+    for i in range(l, r + 1):
+        for j in range(i, r + 1):
+            a = max(a, i ^ j)
+    return a
+
+
+if __name__ == "__main__":
+    l = int(input().strip())
+    r = int(input().strip())
+    print(l, r)
+    result = maximizingXor(l, r)
+    print(result)
