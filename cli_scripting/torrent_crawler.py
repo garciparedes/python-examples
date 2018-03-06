d3737ac0927f9e8cfa35199b9e0f4a549fad2d5b
Sergio Garcia Prado <sergio@garciparedes.me>
2018-03-06 16:37:14 +0100

    Nothing

--- cli_scripting/torrent_crawler.py
+++ cli_scripting/torrent_crawler.py
@@ -12,7 +12,7 @@ def main():
     opener = req.build_opener()
     opener.addheaders = [('User-agent', 'Mozilla/5.0')]
     req.install_opener(opener)
-    page =  req.urlopen(url).read().decode('utf-8')
+    page = req.urlopen(url).read().decode('utf-8')
     links = re.findall(r'"([^"]+\.torrent)"', page)
     for i, url in enumerate(links):
         file_name = "torrent_" + str(i) + ".torrent"
