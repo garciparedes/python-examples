a016c39e981c3809d3c44d907b5ba07b2c824468
Sergio Garcia Prado <sergio@garciparedes.me>
2018-03-18 13:37:23 +0100

    Done Extra Long Factorials Exercise

---
+++ hacker_rank/extra_long_factorials.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+
+import sys
+
+def extraLongFactorials(n):
+    fact = 1
+    for i in range(2, n + 1):
+        fact *= i
+    print(fact)
+
+if __name__ == "__main__":
+    n = int(input().strip())
+    extraLongFactorials(n)
