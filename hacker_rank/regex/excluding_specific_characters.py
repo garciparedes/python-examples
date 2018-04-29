35a0c130d5cf247133dee3da91651307949402c3
Sergio Garcia Prado <sergio@garciparedes.me>
2018-04-29 11:56:06 +0200

    Done with Excluding Specific Characters exercise

---
+++ hacker_rank/regex/excluding_specific_characters.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+
+import sys
+import re
+
+
+def main():
+    regex_pattern = r'^[^\d][^aeiou][^bcDF][^\r\n\t ][^AEIOU][^\.,]$'
+    print(str(bool(re.search(regex_pattern, input()))).lower())
+
+
+if __name__ == '__main__':
+    main()
