8cc99c2eac1141a81de881d2bc558aac1348fc4c
Sergio Garcia Prado <sergio@garciparedes.me>
2018-05-01 12:17:06 +0200

    Done with Matching Character Ranges exercise

---
+++ hacker_rank/regex/matching_range_of_characters.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+
+import sys
+import re
+
+
+def main():
+    regex_pattern = r'^[a-z][1-9][^a-z][^A-Z][A-Z]'
+    print(str(bool(re.search(regex_pattern, input()))).lower())
+
+
+if __name__ == '__main__':
+    main()
