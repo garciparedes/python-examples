cfc7ffff8b0a05dba11f3d660aea9bcf188e33c9
Sergio Garcia Prado <sergio@garciparedes.me>
2018-05-04 13:26:26 +0200

    Done with Matching {x} Repetitions exercise

---
+++ hacker_rank/regex/matching_x_repetions.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+
+import sys
+import re
+
+
+def main():
+    regex_pattern = r'^[a-zA-Z02468]{40}[13579\s]{5}$'
+    print(str(bool(re.search(regex_pattern, input()))).lower())
+
+
+if __name__ == '__main__':
+    main()
