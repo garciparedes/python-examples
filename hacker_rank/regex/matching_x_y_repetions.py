d3e8521636f532a37c928cb54c19edf2bd8d6f13
Sergio Garcia Prado <sergio@garciparedes.me>
2018-05-06 10:36:12 +0200

    Done with Matching {x, y} Repetitions exercise

---
+++ hacker_rank/regex/matching_x_y_repetions.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+
+import sys
+import re
+
+
+def main():
+    regex_pattern = r'^\d{1,2}[a-zA-Z]{3,}\.{,3}$'
+    print(str(bool(re.search(regex_pattern, input()))).lower())
+
+
+if __name__ == '__main__':
+    main()
