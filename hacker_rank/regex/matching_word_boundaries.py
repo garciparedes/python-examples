701950ab8184394300de853f8075d59d6f2d711c
Sergio Garcia Prado <sergio@garciparedes.me>
2018-05-17 08:56:41 +0200

    Done with Matching Word Boundaries

---
+++ hacker_rank/regex/matching_word_boundaries.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+
+import sys
+import re
+
+
+def main():
+    regex_pattern = r'\b[aeiouAEIOU][a-zA-Z]*\b'
+    print(str(bool(re.search(regex_pattern, input()))).lower())
+
+
+if __name__ == '__main__':
+    main()
