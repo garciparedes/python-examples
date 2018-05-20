80528fb1f5fe87c07e53f492bb6087ed097de1cb
Sergio Garcia Prado <sergio@garciparedes.me>
2018-05-20 08:50:25 +0200

    Done with Positive Lookahead exercise

---
+++ hacker_rank/regex/positive_lookahead.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+
+import sys
+import re
+
+
+def main():
+    regex_pattern = r'o(?=oo)'
+    print(str(bool(re.search(regex_pattern, input()))).lower())
+
+
+if __name__ == '__main__':
+    main()
