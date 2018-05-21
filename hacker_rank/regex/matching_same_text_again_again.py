0466013a1355d5bdd2c162f50eae1049d6095ea5
Sergio Garcia Prado <sergio@garciparedes.me>
2018-05-21 09:03:51 +0200

    Done with Matching Same Text Again & Again exercise

---
+++ hacker_rank/regex/matching_same_text_again_again.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+
+import sys
+import re
+
+
+def main():
+    regex_pattern = r'^([a-z]\w\s\W\d\D[A-Z][a-zA-Z][aeiouAEIOU]\S)\1$'
+    print(str(bool(re.search(regex_pattern, input()))).lower())
+
+
+if __name__ == '__main__':
+    main()
