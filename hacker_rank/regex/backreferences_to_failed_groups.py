d2226ddea43a0b830ddf225e98efad7c4e25a863
Sergio Garcia Prado <sergio@garciparedes.me>
2018-05-22 09:20:57 +0200

    Done with Backreferences To Failed Groups exercise

---
+++ hacker_rank/regex/backreferences_to_failed_groups.py
@@ -0,0 +1,12 @@
+#!/bin/python3
+
+import re
+
+
+def main() -> None:
+    regex_pattern = r'^\d{2}(-?)(\d{2}\1){2}\d{2}$'
+    print(str(bool(re.search(regex_pattern, input()))).lower())
+
+
+if __name__ == '__main__':
+    main()
