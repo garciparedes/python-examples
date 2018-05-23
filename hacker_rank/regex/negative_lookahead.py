1a1c08de3690b8f71590f21e4656bc57b01b75f2
Sergio Garcia Prado <sergio@garciparedes.me>
2018-05-23 09:17:38 +0200

    Done with Negative Lookahead exercise

---
+++ hacker_rank/regex/negative_lookahead.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+
+import re
+
+
+def main() -> None:
+    regex_pattern = r'(.){1}(?!\1)'
+
+    match = re.findall(Regex_Pattern, input())
+
+    print("Number of matches :", len(match))
+
+
+if __name__ == '__main__':
+    main()
