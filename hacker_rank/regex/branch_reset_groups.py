1ff6248968b84eb724104cbc8752b9bf4fd22dee
Sergio Garcia Prado <sergio@garciparedes.me>
2018-05-24 09:09:16 +0200

    Done with Branch Reset Groups

---
+++ hacker_rank/regex/branch_reset_groups.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+
+import re
+
+REGEX_PATTERN = r'^\d{2}(((---)|(-)|(.)|(:))?)(\d{2}\1){2}\d{2}$'
+
+def is_matched(s: str) -> bool:
+    return bool(re.search(REGEX_PATTERN, s))
+
+def main() -> None:
+    print(str(is_matched(input())).lower())
+
+
+if __name__ == '__main__':
+    main()
