6e13aab3d2f3523aef0458747b82444fdd294768
Sergio Garcia Prado <sergio@garciparedes.me>
2018-05-25 17:55:33 +0200

    Done with Forward References exercise (adapted to python)

---
+++ hacker_rank/regex/forward_references.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+
+import re
+
+REGEX_PATTERN = r'^(tac)(\1(tic)?)*$'
+
+def is_matched(s: str) -> bool:
+    return bool(re.search(REGEX_PATTERN, s))
+
+def main() -> None:
+    print(str(is_matched(input())).lower())
+
+
+if __name__ == '__main__':
+    main()
