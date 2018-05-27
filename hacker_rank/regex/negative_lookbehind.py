83918d96a3fd75170fcf6abf99a2943387b56cb3
Sergio Garcia Prado <sergio@garciparedes.me>
2018-05-27 11:33:44 +0200

    Done with Negative Lookbehind exercise

---
+++ hacker_rank/regex/negative_lookbehind.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+
+import re
+
+REGEX_PATTERN = r'(?<![aeiouAEIOU]).'
+
+def find_matches(s: str):
+    re.findall(REGEX_PATTERN, s)
+
+def main() -> None:
+    print("Number of matches :", len(find_matches(input())))
+
+
+if __name__ == '__main__':
+    main()
