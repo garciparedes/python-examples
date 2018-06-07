1a03287197e64ff5d526b6e94f4a2ec15a04ab50
Sergio Garcia Prado <sergio@garciparedes.me>
2018-06-07 12:48:20 +0200

    Done with Alien Username exercise

---
+++ hacker_rank/regex/applications/alien_username.py
@@ -0,0 +1,24 @@
+#!/bin/python3
+
+import re
+
+import enforce
+
+REGEX_PATTERN = r'[\._]\d+[a-zA-Z]*_?$'
+
+
+@enforce.runtime_validation
+def validate(s: str) -> bool:
+    return re.match(REGEX_PATTERN, s) is not None
+
+
+@enforce.runtime_validation
+def main() -> None:
+    n = int(input())
+    s = ""
+    for i in range(n):
+        print("VALID" if validate(input()) else "INVALID")
+
+
+if __name__ == '__main__':
+    main()
