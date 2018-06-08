9c18f812df985d4caf806250af30a40cb2b7d9fd
Sergio Garcia Prado <sergio@garciparedes.me>
2018-06-08 09:39:14 +0200

    Done with Finding the percentage exercise

---
+++ hacker_rank/python/data_type/finding_the_percentage.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import statistics as s
+
+import enforce
+
+
+@enforce.runtime_validation
+def main() -> None:
+    arr = dict()
+    for _ in range(int(input())):
+        e = input().split()
+        arr[e[0]] = map(float, e[1:])
+    print(f'{s.mean(arr[input()]):.2f}')
+
+
+if __name__ == '__main__':
+    main()
