aba351de11fe15efb2066eb6f7c4b8f52a1cd4a7
Sergio Garcia Prado <sergio@garciparedes.me>
2018-06-09 10:05:43 +0200

    Done with Python Mutations Challenge

---
+++ hacker_rank/python/string/python_mutations.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import enforce
+
+
+@enforce.runtime_validation
+def mutate_string(s: str, pos: int, ch: chr) -> str:
+    return s[:pos] + ch + s[pos + 1:]
+
+
+@enforce.runtime_validation
+def main() -> None:
+    s = input()
+    i, c = input().split()
+    print(mutate_string(s, int(i), c))
+
+
+if __name__ == '__main__':
+    main()
