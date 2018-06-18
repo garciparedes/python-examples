e5d96bc5ab1f0bded02c3ea2543341bedb4a474a
Sergio García Prado <garciparedes@TheCUBE.es>
2018-06-18 12:21:58 +0200

    Done with Itertools - Combinations exercise

---
+++ hacker_rank/python/itertools/combinations.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import itertools as it
+
+import enforce
+
+
+@enforce.runtime_validation
+def main() -> None:
+    s, n = input().split(' ')
+    s = sorted(s)
+    n = int(n)
+    print('\n'.join(
+        map(lambda x: ''.join(x),
+            it.chain(
+                *map(lambda i: it.combinations(s, i),
+                     range(1, n + 1))
+            )
+        )
+    ))
+
+
+if __name__ == '__main__':
+    main()
