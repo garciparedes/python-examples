799ef76a2e9d3f585ce7f5bd21e795957e5772fd
Sergio García Prado <garciparedes@TheCUBE.es>
2018-06-18 11:57:36 +0200

    Done with Itertools - Permutations exercise

---
+++ hacker_rank/python/itertools/permutations.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import itertools as it
+
+import enforce
+
+
+@enforce.runtime_validation
+def main() -> None:
+    s, n = input().split(' ')
+    print('\n'.join(map(lambda x: ''.join(x), it.permutations(sorted(s), int(n)))))
+
+
+if __name__ == '__main__':
+    main()
