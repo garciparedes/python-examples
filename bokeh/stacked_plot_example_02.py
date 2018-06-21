6b5eb86d51d4f33b4c1872cee68adc1ffbaf967d
garciparedes <sergiogp@unlimiteck.com>
2018-06-21 12:09:27 +0200

    Added example

---
+++ bokeh/stacked_plot_example_02.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+
+# EXAMPLE FROM: https://bokeh.pydata.org/en/latest/docs/user_guide/categorical.html#stacked
+
+from bokeh.core.properties import value
+from bokeh.io import show
+from bokeh.plotting import figure
+
+
+def main() -> None:
+
+    fruits = ['Apples', 'Pears', 'Nectarines', 'Plums', 'Grapes', 'Strawberries']
+    years = ["2015", "2016", "2017"]
+    colors = ["#c9d9d3", "#718dbf", "#e84d60"]
+
+    data = {'fruits' : fruits,
+            '2015'   : [2, 1, 4, 3, 2, 4],
+            '2016'   : [5, 3, 4, 2, 4, 6],
+            '2017'   : [3, 2, 4, 4, 5, 3]}
+
+    p = figure(x_range=fruits, plot_height=250, title="Fruit Counts by Year",
+               toolbar_location=None, tools="")
+
+    p.vbar_stack(years, x='fruits', width=0.9, color=colors, source=data,
+                 legend=[value(x) for x in years])
+
+    p.y_range.start = 0
+    p.x_range.range_padding = 0.1
+    p.xgrid.grid_line_color = None
+    p.axis.minor_tick_line_color = None
+    p.outline_line_color = None
+    p.legend.location = "top_left"
+    p.legend.orientation = "horizontal"
+
+    show(p)
+
+if __name__ == '__main__':
+    main()
