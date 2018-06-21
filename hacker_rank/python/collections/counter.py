8c51eaed4a1e0b89bfe3133b05dae1f87e298254
garciparedes <sergiogp@unlimiteck.com>
2018-06-21 12:53:48 +0200

    Done with 'Python - Counter' exercise

---
+++ hacker_rank/python/collections/counter.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+from collections import Counter
+from functools import reduce
+import enforce
+
+
+@enforce.runtime_validation
+def f(cnt: Counter, shape: str, price) -> int:
+    if cnt[shape] > 0:
+        cnt[shape] -= 1
+        return int(price)
+    else:
+        return 0
+
+
+@enforce.runtime_validation
+def main() -> None:
+    _ = int(input())
+    x = input().split()
+    cnt = Counter(x)
+    c_n = int(input())
+    print(reduce(lambda a, b: a + b,
+                 map(lambda _: f(cnt, *input().split()),
+                     range(c_n))
+                 )
+          )
+
+
+if __name__ == '__main__':
+    main()
