fa9fc9055a9b992293afb6cfbaa5fa7e18e68322
Sergio García <garciparedes@gmail.com>
2018-06-27 09:13:44 +0200

    Done with 'Python - defaultdict' exercise

---
+++ hacker_rank/python/collections/default_dict.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+from collections import defaultdict
+import enforce
+
+
+@enforce.runtime_validation
+def main() -> None:
+    n, m = map(int, input().strip().split())
+    d = defaultdict(list)
+
+    for i in range(1, n + 1):
+        d[input().strip()].append(i)
+
+    for i in range(m):
+        print(' '.join(map(str, d[input().strip()] or [-1])))
+
+
+if __name__ == '__main__':
+    main()
