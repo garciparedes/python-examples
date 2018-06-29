e47d22aeef18ba13ddf5d24c026c5c6f6ac7dd98
Sergio García <garciparedes@gmail.com>
2018-06-29 09:03:53 +0200

    Done with 'Python - NamedTuple' exercise

---
+++ hacker_rank/python/collections/named_tuple.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from collections import namedtuple
+from statistics import mean
+import enforce
+
+
+@enforce.runtime_validation
+def main() -> None:
+    n = int(input().strip())
+    Student = namedtuple('Student', input().strip().split())
+
+    list_students = list()
+    for i in range(n):
+        list_students.append(Student(*input().strip().split()))
+
+    print('{:.2f}'.format(mean(map(lambda x: int(x.MARKS), list_students))))
+
+
+if __name__ == '__main__':
+    main()
