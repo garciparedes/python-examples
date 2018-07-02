3f86cd0338b64abe5e4bf6b41231f3781550b081
Sergio García <garciparedes@gmail.com>
2018-07-02 09:12:41 +0200

    Done with 'Python - OrderedDict' exercise

---
+++ hacker_rank/python/collections/ordered_dict.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+from collections import OrderedDict
+import enforce
+
+
+@enforce.runtime_validation
+def main() -> None:
+    n = int(input().strip())
+
+    dict_products = OrderedDict()
+    for i in range(n):
+        raw = input().strip().split(' ')
+        price = int(raw[-1])
+        name = ' '.join(raw[:-1])
+        if name in dict_products:
+            dict_products[name] += int(price)
+        else:
+            dict_products[name] = int(price)
+
+    for name, price in dict_products.items():
+        print(name, price, sep=' ')
+
+
+if __name__ == '__main__':
+    main()
