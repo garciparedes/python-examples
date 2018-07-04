36a46d41d9d64db9db63ed1dabbc44ed01156fc2
Sergio García <garciparedes@gmail.com>
2018-07-04 09:27:34 +0200

    Done with 'Python - No idea' exercise

---
+++ hacker_rank/python/set/no_idea.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+
+import enforce
+
+import collections
+
+
+
+
+@enforce.runtime_validation
+def calculate_happiness(cnt_arr, set_a, set_b) -> int:
+    happiness = 0
+    for v, n in cnt_arr.most_common():
+        if v in set_a:
+            happiness += n
+        elif v in set_b:
+            happiness -= n
+    return happiness
+
+
+@enforce.runtime_validation
+def main() -> None:
+    n, m = map(int, input().strip().split())
+    cnt_arr = collections.Counter(input().strip().split())
+    set_a = frozenset(input().strip().split())
+    set_b = frozenset(input().strip().split())
+    # assert(n == len(cnt_arr) and m == len(set_a) and m == len(set_b))
+
+    print(calculate_happiness(cnt_arr, set_a, set_b))
+
+
+if __name__ == '__main__':
+    main()
