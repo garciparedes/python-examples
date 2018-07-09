2160a36d2c34525cd83bedacd3b59ec854503dd0
Sergio García <garciparedes@gmail.com>
2018-07-09 09:13:29 +0200

    Done with 'Python - Set Add' exercise

---
+++ hacker_rank/python/set/set_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+
+def main() -> None:
+    s = set()
+    for _ in range(int(input().strip())):
+        s.add(input().strip())
+    print(len(s))
+
+if __name__ == '__main__':
+    main()
