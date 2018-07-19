c443f66abb240762d7758e471ea986aa9c0e8a9e
garciparedes <sergiogp@unlimiteck.com>
2018-07-19 10:17:35 +0200

    Done with 'Python - Mod Divmod' exercise.

---
+++ hacker_rank/python/math/mod_divmod.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import itertools as it
+
+
+def read_int() -> int:
+    while True:
+        yield int(input().strip())
+
+
+def main() -> None:
+    a, b = it.islice(read_int(), 2)
+
+    print(a // b, a % b, divmod(a, b), sep="\n")
+
+
+if __name__ == '__main__':
+    main()
