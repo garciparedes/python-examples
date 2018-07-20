53730ebf47e4339911c60aca6a3b515c844f5037
garciparedes <sergiogp@unlimiteck.com>
2018-07-20 09:11:52 +0200

    Done with 'Python - Power ModPower' exercise.

---
+++ hacker_rank/python/math/power_modpower.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from typing import Iterable
+import itertools as it
+
+
+def read_int() -> Iterable[int]:
+    while True:
+        yield int(input().strip())
+
+
+def main() -> None:
+    a, b, m = it.islice(read_int(), 3)
+
+    print(pow(a, b), pow(a,b,m), sep="\n")
+
+
+if __name__ == '__main__':
+    main()
