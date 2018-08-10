d572ebd13a95011965ce5d1a2170cb6cededcd17
sergiogp <sergiogp@unlimiteck.com>
2018-08-10 09:26:06 +0200

    Minor improvement.

--- hacker_rank/python/classes/dealing_with_complex_numbers.py
+++ hacker_rank/python/classes/dealing_with_complex_numbers.py
@@ -37,9 +37,13 @@ class Complex(object):
         return result
 
 
-if __name__ == '__main__':
+def main():
     c = map(float, input().split())
     d = map(float, input().split())
     x = Complex(*c)
     y = Complex(*d)
     print(*map(str, [x + y, x - y, x * y, x / y, x.mod(), y.mod()]), sep='\n')
+
+
+if __name__ == '__main__':
+    main()
