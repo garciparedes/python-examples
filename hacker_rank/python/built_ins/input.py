a285253a849e2747f308492b93c149e122565a29
Sergio Garcia Prado <sergio@garciparedes.me>
2018-08-12 15:44:26 +0200

    Done with 'Python - Built-Ins: Input()' exercise

---
+++ hacker_rank/python/built_ins/input.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+from typing import List, Iterable
+
+
+def read_str() -> str:
+    return input().strip()
+
+
+def read_str_line_iterator() -> List[str]:
+    return read_str().split(' ')
+
+
+def read_int_line_iterator() -> Iterable[int]:
+    return map(int, read_str_line_iterator())
+
+
+def eval_polynomial(expression: str, x: int) -> int:
+    return eval(expression)
+
+
+def main() -> None:
+    x, k = read_int_line_iterator()
+    expression = read_str()
+    result = eval_polynomial(expression, x)
+    print(result == k)
+
+
+if __name__ == '__main__':
+    main()
