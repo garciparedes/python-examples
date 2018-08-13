058af7141ca47d0ce42c87e047ef00d438192d91
sergiogp <sergiogp@unlimiteck.com>
2018-08-13 09:05:58 +0200

    Done with 'Python - Built-Ins: eval()' exercise.

---
+++ hacker_rank/python/built_ins/eval.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+def read_str() -> str:
+    return input().strip()
+
+
+def eval_expression(expression: str) -> None:
+    eval(expression)
+
+
+def main() -> None:
+    expression = read_str()
+    eval_expression(expression)
+
+
+if __name__ == '__main__':
+    main()
