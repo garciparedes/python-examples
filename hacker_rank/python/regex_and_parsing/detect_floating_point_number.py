4e9abbe375d33fecb6a563f50f51a4787e4e94f6
sergiogp <sergiogp@unlimiteck.com>
2018-08-20 16:00:45 +0200

    Minor changes

--- hacker_rank/python/regex_and_parsing/detect_floating_point_number.py
+++ hacker_rank/python/regex_and_parsing/detect_floating_point_number.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
 
 import re
+import itertools as it
+
 
 FLOATING_POINT_REGEX = re.compile(r'^[+\-]?\d*\.\d+$')
 
 
 def read_str() -> str:
     return input().strip()
+
+
+def yield_str() -> str:
+    while True:
+        yield input().strip()
 
 
 def read_int() -> int:
@@ -18,9 +25,9 @@ def is_floating_point_number(text: str) -> bool:
 
 
 def main() -> None:
-    for _ in range(read_int()):
-        text = read_str()
-        print(is_floating_point_number(text))
+    possible_floats = it.islice(yield_str(), read_int())
+    are_floats = map(lambda text: str(is_floating_point_number(text)), possible_floats)
+    print('\n'.join(are_floats))
 
 
 if __name__ == '__main__':
