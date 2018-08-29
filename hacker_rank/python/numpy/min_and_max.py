88a0a8463164d5dc17863e0b2926deda2e8af0d5
sergiogp <sergiogp@unlimiteck.com>
2018-08-29 16:10:41 +0200

    Done with 'Python - Numpy: Min and Max' exercise.

---
+++ hacker_rank/python/numpy/min_and_max.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+from typing import List, Iterable
+
+import numpy as np
+
+
+def read_str() -> str:
+    return input().strip()
+
+
+def read_str_list() -> List[str]:
+    return read_str().split(' ')
+
+
+def read_int_iterable() -> Iterable[int]:
+    return map(int, read_str_list())
+
+
+def read_int_list() -> List[int]:
+    return list(read_int_iterable())
+
+
+def read_array(n: int, m: int) -> np.ndarray:
+    raw_data = []
+    for _ in range(n):
+        raw_data.append(read_int_list())
+    data = np.array(raw_data)
+    return data
+
+
+def main() -> None:
+    n, m = read_int_list()
+    data = read_array(n, m)
+    result = data.min(axis=1).max(axis=0)
+    print(result)
+
+
+if __name__ == '__main__':
+    main()
