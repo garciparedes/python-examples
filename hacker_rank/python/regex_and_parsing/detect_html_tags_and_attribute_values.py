f749325240ff66d6201493152949399202aee569
Sergio Garcia Prado <sergio@garciparedes.me>
2018-09-15 12:03:34 +0200

    Done with 'Python - Regex and Parsing: Detect HTML Tags, Attributes and Attribute Values' exercise.

---
+++ hacker_rank/python/regex_and_parsing/detect_html_tags_and_attribute_values.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+
+from html.parser import HTMLParser
+
+
+# create a subclass and override the handler methods
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        out = str()
+        out += '{}\n'.format(tag)
+        for attr in attrs:
+            out += '-> {} > {}\n'.format(*attr)
+        print(out, end='')
+
+    def handle_startendtag(self, tag, attrs):
+        out = str()
+        out += ('{}\n'.format(tag))
+        for attr in attrs:
+            out += '-> {} > {}\n'.format(*attr)
+        print(out, end='')
+
+
+def read_str() -> str:
+    return input().strip()
+
+
+def read_int() -> int:
+    return int(read_str())
+
+
+def show_html_stats(html_str: str) -> None:
+    parser = MyHTMLParser()
+    parser.feed(html_str)
+
+
+def main() -> None:
+    text = str()
+    for _ in range(read_int()):
+        text += read_str()
+    show_html_stats(text)
+
+
+if __name__ == '__main__':
+    main()
