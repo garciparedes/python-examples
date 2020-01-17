d8a16949f1a3036a51b45c6825bcbc4d8e74200f
garciparedes <sergio@garciparedes.me>
2020-01-17 12:13:56 +0100

    * Added read example.

--- numerical/arrow/csv_to_parquet.py
+++ numerical/arrow/csv_to_parquet.py
@@ -1,28 +1,48 @@
 """ From: 'https://stackoverflow.com/a/45618618/3921457' """
+import logging
+
 import pandas as pd
 import pyarrow as pa
 import pyarrow.parquet as pq
 from pathlib import Path
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 CSV_FILE_PATH = Path(__file__).parents[1] / 'data.csv'
 PARQUET_FILE_PATH = Path(__file__).parents[1] / 'data.parquet'
+
 CHUNK_SIZE = 100_000
 
 
 def main():
-    csv_stream = pd.read_csv(CSV_FILE_PATH, chunksize=CHUNK_SIZE, low_memory=False)
+    logger.info(f'Starting...')
+
+    csv_stream = pd.read_csv(
+        CSV_FILE_PATH,
+        chunksize=CHUNK_SIZE,
+        low_memory=False,
+        sep='|',
+        decimal=',',
+        encoding='latin-1',
+    )
+
+    logger.info(f'CSV Stored Size: {CSV_FILE_PATH.stat().st_size / 1024 ** 3:.3f} GB')
 
     chunk = next(csv_stream)
+    logger.debug(f'Processing 1-th chunk...')
     parquet_schema = pa.Table.from_pandas(chunk).schema
-
     parquet_writer = pq.ParquetWriter(PARQUET_FILE_PATH, parquet_schema, compression='snappy')
 
-    for i, chunk in enumerate(csv_stream):
-        print("Chunk", i)
+    for i, chunk in enumerate(csv_stream, 2):
+        logger.debug(f'Processing {i}-th chunk...')
         table = pa.Table.from_pandas(chunk, parquet_schema)
         parquet_writer.write_table(table)
 
     parquet_writer.close()
+    logger.info(f'Parquet Stored Size: {PARQUET_FILE_PATH.stat().st_size / 1024 ** 3:.3f} GB')
+
+    logger.info(f'Finished!')
 
 
 if __name__ == '__main__':
--- numerical/arrow/read.py
+++ numerical/arrow/read.py
@@ -6,19 +6,16 @@ from pathlib import Path
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-CSV_FILE_PATH = Path(__file__).parents[1] / 'data.csv'
 PARQUET_FILE_PATH = Path(__file__).parents[1] / 'data.parquet'
 
 
 def main():
     logger.info(f'Starting...')
 
-    file_path = Path(CSV_FILE_PATH)
-    df = pd.DataFrame({'a': [1, 2, 3, 4, 5], 'b': [6, 7, 8, 9, 10]})
-    df.to_parquet(file_path)
+    logger.info(f'Parquet Stored Size: {PARQUET_FILE_PATH.stat().st_size / 1024 ** 3:.3f} GB')
 
-    df = pd.read_parquet(file_path)
-    logger.info(df)
+    df = pd.read_parquet(PARQUET_FILE_PATH)
+    logger.info(f'In memory Size: {df.memory_usage(deep=True).sum() / 1024 ** 3:.3f} GB')
 
     logger.info(f'Finished!')
 
