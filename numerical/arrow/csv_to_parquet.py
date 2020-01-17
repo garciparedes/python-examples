1cfe9b62289e8ce9e9dbf94dc11f15816e389156
garciparedes <sergio@garciparedes.me>
2020-01-17 11:48:33 +0100

    * Minor improvements.

--- numerical/arrow/csv_to_parquet.py
+++ numerical/arrow/csv_to_parquet.py
@@ -4,18 +4,18 @@ import pyarrow as pa
 import pyarrow.parquet as pq
 from pathlib import Path
 
-csv_file = Path('df.csv')
-parquet_file = Path('df.parquet')
-CHUNK_SIZE = 1
+CSV_FILE_PATH = Path(__file__).parents[1] / 'data.csv'
+PARQUET_FILE_PATH = Path(__file__).parents[1] / 'data.parquet'
+CHUNK_SIZE = 100_000
 
 
 def main():
-    csv_stream = pd.read_csv(csv_file, chunksize=CHUNK_SIZE, low_memory=False)
+    csv_stream = pd.read_csv(CSV_FILE_PATH, chunksize=CHUNK_SIZE, low_memory=False)
 
     chunk = next(csv_stream)
     parquet_schema = pa.Table.from_pandas(chunk).schema
 
-    parquet_writer = pq.ParquetWriter(parquet_file, parquet_schema, compression='snappy')
+    parquet_writer = pq.ParquetWriter(PARQUET_FILE_PATH, parquet_schema, compression='snappy')
 
     for i, chunk in enumerate(csv_stream):
         print("Chunk", i)
